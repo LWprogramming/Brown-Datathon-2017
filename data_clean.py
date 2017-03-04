c0cab0e103bd953bb36619ab0a22e0e39d268b5d
Leon Wu <lwgithub@gmail.com>
2017-03-04 13:06:51 -0500

    Create clean_data method

--- data_clean.py
+++ data_clean.py
@@ -2,7 +2,16 @@ import numpy as np
 import pandas
 import os
 
-def data_path():
+def get_data_path():
 	parent_path = os.path.pardir
 	data_path = parent_path + "/TripAdvisorChallenge/datathon_tadata.csv"
 	return data_path
+
+def clean_data():
+	# print(get_data_path())
+	data = pandas.read_csv(get_data_path()).as_matrix()
+	return data
+
+if __name__ == '__main__':
+	data = clean_data()
+	print(data[0, 1]) # should be 2017-01-10
