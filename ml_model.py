5e3920f76668003888466b3f881c57f6ed7cd3a8
Leon Wu <lwgithub@gmail.com>
2017-03-04 17:57:19 -0500

    Add some new models to try out

--- ml_model.py
+++ ml_model.py
@@ -1,18 +1,30 @@
 import ipdb
 import numpy as np
-from sklearn import svm # going to start with svm to try out.
+# from sklearn import svm # going to start with svm to try out.
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.linear_model import LogisticRegression
 
 if __name__ == '__main__':
     data = np.load('cleaned_data.npy')
-    data = data[:1000, :]
+    # data = data[:1000, :]
     labels = data[:, 0]
     features = data[:, 1:]
 
-    feature_train, feature_test, label_train, label_test = train_test_split(features, labels, test_size=0.4, random_state=0)
+    # cv is about 0.77
+    # gnb = GaussianNB()
+    # scores = cross_val_score(gnb, features, labels, cv=5)
+    # print(np.mean(scores))
 
-    clf = svm.SVC(kernel='linear', C=1).fit(feature_train, label_train)
+    # cv is about 0.68
+    # mnb = MultinomialNB()
+    # scores = cross_val_score(mnb, features, labels, cv=5)
+    # print(np.mean(scores))
 
-    # ipdb.set_trace()  ######### Break Point ###########
+    # cv is about 0.78 (TAKES A LONG TIME TO TRAIN)
+    # clf = LogisticRegression(solver='lbfgs', multi_class='multinomial')
+    # scores = cross_val_score(clf, features, labels, cv=5)
+    # print(np.mean(scores))
 
-    print(clf.score(feature_test, label_test))
+    
