32567ed1783ae859908c96298d090b826c6c3d04
Leon Wu <lwgithub@gmail.com>
2017-03-04 13:01:18 -0500

    Add data path method

--- data_clean.py
+++ data_clean.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas
 import os
+
+def data_path():
+	parent_path = os.path.pardir
+	data_path = parent_path + "/TripAdvisorChallenge/datathon_tadata.csv"
+	return data_path
